b5da13c590bc3c714b1bd07c31e88411a4c6cc9e
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-11 16:14:39 +0530

    Refractorusing switch case statement for solving in usecase4

--- src/com/oops/empwage/EmpWage.java
+++ src/com/oops/empwage/EmpWage.java
@@ -2,33 +2,40 @@ package com.oops.empwage;
 
 public class EmpWage {
 
+	final static int IS_FULL_TIME = 1;
+	final static int IS_PART_TIME = 2;
+	final static int WAGE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		// Check Employee is Present or Absent UC-1
-		int is_full_time = 1;
-		int is_part_time = 2;
-		int wagePerHour = 20;
 		int hours = 0;
 		int attendanceCheck = (int) Math.floor((Math.random() * 10) % 3);
 
-		if (attendanceCheck == is_full_time) {
-			System.out.println("employee is present and full time");
+		switch (attendanceCheck) {
+		case IS_FULL_TIME:
 			hours = 8;
-		} else if (attendanceCheck == is_part_time) {
-			System.out.println("employee is present and part time");
+			System.out.println("employee is present and full time");
+			break;
+		case IS_PART_TIME:
 			hours = 4;
-		} else {
+			System.out.println("employee is present and part time");
+			break;
+		default:
 			System.out.println("employee is absent");
-
+			break;
 		}
+
 		// Calculate Daily Employee Wage UC-2
 
-		int dailyEmpWage = hours * wagePerHour;
+		int dailyEmpWage = hours * WAGE_PER_HOUR;
 		System.out.println("daily employee wage =" + dailyEmpWage);
-		
-		//Add Part time Employee & Wage UC-3
+
+		// Add Part time Employee & Wage UC-3
+
+		// Solving using Switch Case Statement UC-4
 	}
 
 }
