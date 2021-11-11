eba6c45be34951b575e28a36ca907502bc52b773
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-11 16:00:14 +0530

    Adding calculation of employee daily wage in usecase2

--- src/com/oops/empwage/EmpWage.java
+++ src/com/oops/empwage/EmpWage.java
@@ -8,13 +8,21 @@ public class EmpWage {
 
 		// Check Employee is Present or Absent UC-1
 		int is_present = 1;
+		int wagePerHour = 20;
+		int hours = 0;
 		int attendanceCheck = (int) Math.floor((Math.random() * 10) % 2);
 
 		if (attendanceCheck == is_present) {
 			System.out.println("employee is present");
+			hours=8;
 		} else {
 			System.out.println("employee is absent");
 		}
+
+		// Calculate Daily Employee Wage UC-2
+
+		int dailyEmpWage = hours * wagePerHour;
+		System.out.println("daily employee wage =" + dailyEmpWage);
 	}
 
 }
