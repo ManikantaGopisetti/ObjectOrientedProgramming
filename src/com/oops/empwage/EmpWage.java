85db8d98b885912005d1de07e07a418fc608b703
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-11 16:34:58 +0530

    Adding calculation of wages per month in usecase5

--- src/com/oops/empwage/EmpWage.java
+++ src/com/oops/empwage/EmpWage.java
@@ -5,37 +5,45 @@ public class EmpWage {
 	final static int IS_FULL_TIME = 1;
 	final static int IS_PART_TIME = 2;
 	final static int WAGE_PER_HOUR = 20;
+	final static int MAX_WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Employee Wage Computation Program");
-
-		// Check Employee is Present or Absent UC-1
 		int hours = 0;
-		int attendanceCheck = (int) Math.floor((Math.random() * 10) % 3);
+		int noOfWorkDays = 0;
+		int totalWage = 0;
 
-		switch (attendanceCheck) {
-		case IS_FULL_TIME:
-			hours = 8;
-			System.out.println("employee is present and full time");
-			break;
-		case IS_PART_TIME:
-			hours = 4;
-			System.out.println("employee is present and part time");
-			break;
-		default:
-			System.out.println("employee is absent");
-			break;
+		while (noOfWorkDays < MAX_WORKING_DAYS) {
+
+			int attendanceCheck = (int) Math.floor((Math.random() * 10) % 3);
+
+			switch (attendanceCheck) {
+
+			case IS_FULL_TIME:
+				hours = 8;
+				System.out.println("employee is present and full time");
+				noOfWorkDays++;
+				break;
+
+			case IS_PART_TIME:
+				hours = 4;
+				System.out.println("employee is present and part time");
+				noOfWorkDays++;
+				break;
+
+			default:
+				System.out.println("employee is absent");
+				hours = 0;
+				noOfWorkDays++;
+				break;
+			}
+
+			int dailyEmpWage = hours * WAGE_PER_HOUR;
+			System.out.println("daily emp wage= " + dailyEmpWage);
+
+			totalWage += dailyEmpWage;
+
 		}
-
-		// Calculate Daily Employee Wage UC-2
-
-		int dailyEmpWage = hours * WAGE_PER_HOUR;
-		System.out.println("daily employee wage =" + dailyEmpWage);
-
-		// Add Part time Employee & Wage UC-3
-
-		// Solving using Switch Case Statement UC-4
+		System.out.println("total wage per month= " + totalWage);
 	}
-
 }
