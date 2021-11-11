f50d998c44ac55290c1c88564ded7cce9d674cf2
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-11 16:03:59 +0530

    Adding part time employee and wage in usecase3

--- src/com/oops/empwage/EmpWage.java
+++ src/com/oops/empwage/EmpWage.java
@@ -7,22 +7,28 @@ public class EmpWage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		// Check Employee is Present or Absent UC-1
-		int is_present = 1;
+		int is_full_time = 1;
+		int is_part_time = 2;
 		int wagePerHour = 20;
 		int hours = 0;
-		int attendanceCheck = (int) Math.floor((Math.random() * 10) % 2);
+		int attendanceCheck = (int) Math.floor((Math.random() * 10) % 3);
 
-		if (attendanceCheck == is_present) {
-			System.out.println("employee is present");
-			hours=8;
+		if (attendanceCheck == is_full_time) {
+			System.out.println("employee is present and full time");
+			hours = 8;
+		} else if (attendanceCheck == is_part_time) {
+			System.out.println("employee is present and part time");
+			hours = 4;
 		} else {
 			System.out.println("employee is absent");
-		}
 
+		}
 		// Calculate Daily Employee Wage UC-2
 
 		int dailyEmpWage = hours * wagePerHour;
 		System.out.println("daily employee wage =" + dailyEmpWage);
+		
+		//Add Part time Employee & Wage UC-3
 	}
 
 }
