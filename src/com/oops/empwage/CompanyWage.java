87540908f1197ad123b48c5f1d58fb3f2a1c643a
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-16 21:11:51 +0530

    Refractor saving total wage for each company name with code hygiene

--- src/com/oops/empwage/CompanyWage.java
+++ src/com/oops/empwage/CompanyWage.java
@@ -4,39 +4,38 @@ import java.util.Scanner;
 
 public class CompanyWage {
 	private int totalWage;
+	private String CompName;
+
+	Scanner sc = new Scanner(System.in);
+
+	public void addComp() {
+
+		System.out.print("Enter the name of Company : ");
+		String name = sc.nextLine();
+
+		System.out.print("Enter max working days of Company : ");
+		int max_working_days = sc.nextInt();
+
+		System.out.print("Enter max working hours of Company : ");
+		int max_working_hours = sc.nextInt();
+
+		System.out.print("Enter wage per hour of Company : ");
+		int wage_per_hour = sc.nextInt();
+
+		EmployeeWageComputation ec = new EmployeeWageComputation(name, max_working_days, max_working_hours,
+				wage_per_hour);
+		int wagePerMonth = ec.employeeWage();
+		totalWage = wagePerMonth;
+		CompName = name;
+	}
+
 	public String getCompName() {
 		return CompName;
 	}
-	public void setCompName(String compName) {
-		CompName = compName;
-	}
-	private String CompName;
-	
-	Scanner sc = new Scanner(System.in);
 
-	
-	public void addComp() {
-		
-		System.out.print("Enter the name of Company : ");
-		String name = sc.nextLine();
-		
-		System.out.print("Enter max working days of Company : ");
-		int max_working_days=sc.nextInt();
-		
-		System.out.print("Enter max working hours of Company : ");
-		int max_working_hours=sc.nextInt();
-		
-		System.out.print("Enter wage per hour of Company : ");
-		int wage_per_hour=sc.nextInt();
-		
-		EmployeeWageComputation ec = new EmployeeWageComputation(name, max_working_days, max_working_hours, wage_per_hour);
-		int wagePerMonth = ec.employeeWage();
-		totalWage= wagePerMonth;
-		CompName=name;
-	}
 	@Override
 	public String toString() {
 		return "CompanyWage [totalWage=" + totalWage + ", CompanyName=" + CompName + "]";
 	}
-	
+
 }
